864335711c8b77c149a77668e233dce9e257a4f0
Sam Clark <samclark2399@gmail.com>
2022-02-14 23:19:47 -0500

    added temporary way to input zipcode and have it retrurn with a google map being made and it being centered on the zipcode. Also returns data of restaurants within specified price point.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 var zipcodeAPI = "3c0ffffa2f1e899bf98403431f25c752";
-
+ 
 var priceInput = document.querySelector("#price-input");
 var zipInput = document.querySelector("#zip-input");
+var latInput = document.querySelector("#lat-input");
+var lngInput = document.querySelector("#lng-input"); 
 
-let map;
-let service;
-let infowindow;
-
+var map;
+var geocoder;
+var service;
+var infowindow;
+ 
 var priceLevelHandler = function(price) {
     if (price == 1) {
         priceInput.value = "";
@@ -27,9 +30,9 @@ var priceLevelHandler = function(price) {
         priceInput.value = "";
         priceInput.value = 4;
         console.log(priceInput.value)
-    }
-}
-
+    };
+};
+ 
 var formSubmitHandler = function(event){
     event.preventDefault();
     var priceLevel = priceInput.value.trim();
@@ -37,109 +40,113 @@ var formSubmitHandler = function(event){
     if (priceLevel && zipcode) {
         if (priceLevel == '1'){
             priceLevel = 1;
-            getCoords(priceLevel, zipcode);
+            getCoords(zipcode);
+            console.log(priceLevel);
         }
         else if (priceLevel == '2') {
             priceLevel = 2;
-            getCoords(priceLevel, zipcode);
+            getCoords(zipcode);
+            console.log(priceLevel);
         }
         else if (priceLevel == '3') {
             priceLevel = 3;
-            getCoords(priceLevel, zipcode);
+            getCoords(zipcode);
+            console.log(priceLevel);
         }
         else if (priceLevel == '4') {
             priceLevel = 4;
-            getCoords(priceLevel, zipcode);
+            getCoords(zipcode);
+            console.log(priceLevel);
         }
+        initMap();
     }
     else {
         $("body").append($('<div>')
         .addClass('notification is-danger has-text-white warning')
         .attr('id', 'warning')
         .text("Please select price range and enter zipe code."));
-
+ 
         $("#warning").append($('<button>')
         .addClass('delete')
         .attr('onclick', 'deleteWarning()'));
-    }
-}
-
-var getCoords = function(priceLevel, zipcode) {
+    };
+};
+ 
+var getCoords = function(zipcode) {
     fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},US&appid=${zipcodeAPI}`)
     .then(function(response){
         response.json().then(function(data){
             if (data.length === 0){
                 zipInput.value = "";
                 priceInput.value = "";
-
+ 
                 $("body").append($('<div>')
                 .addClass('notification is-danger has-text-white warning')
                 .attr('id', 'warning')
                 .text("Please a valid zipcode."));
-
+ 
                 $("#warning").append($('<button>')
                 .addClass('delete')
                 .attr('onclick', 'deleteWarning()'));
             }
             else {
-            dataConverter(data, priceLevel);
+            latInput.value = data.lat;
+            lngInput.value = data.lon;
             zipInput.value = "";
             priceInput.value = "";
             };
         });
     });
 };
+ 
+function initMap() {
+    lat = document.getElementById("lat-input").value;
+    lng = document.getElementById("lng-input").value;
+    var location = new google.maps.LatLng(lat,lng);
+    var priceLevel = document.getElementById("price-input").value;
+    geocoder = new google.maps.Geocoder();
 
-var dataConverter = function(data, priceLevel) {
-    var lat = data.lat
-    var lon = data.lon
-    initMap(lat, lon, priceLevel)
-    
+  map = new google.maps.Map(document.getElementById(`map1`), {
+      center: location,
+      zoom: 10
+    });
+  var request = {
+    location: location,
+    radius: '5000',
+    type: ['restaurant'],
+    minPriceLevel: priceLevel,
+    maxPriceLevel: priceLevel+.5,
+  }
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
-
-var initMap = function(lat, lon, priceLevel) {
-    console.log(lat, lon, priceLevel)
-    var location = new google.maps.LatLng(lat, lon);
-    console.log(location)
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 15
-        });
-    var request = {
-        location: location,
-        radius: '1000',
-        type: ['restaurant'],
-        minPriceLevel: priceLevel,
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
-    }
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 1; i < 5; i++) {
-            console.log(results[i])
-            // $(`#${i}`).text(results[i].rating)
-            // $(`#${i}`).text(results[i].name)
-                //   createMarker(results[i]);
-            }
-        }
-    }
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //   for (var i = 1; i < 5; i++) {
+          console.log(results)
+        //   createMarker(results);
+        //  }
+      }
+      else {
+          console.log('failed' +status)
+      }
+  }
 // function createMarker(place) {
-//   if (!place.geometry || !place.geometry.location) return;
-
-//   const marker = new google.maps.Marker({
-//     map,
-//     position: place.geometry.location,
-//   });
-
-//   google.maps.event.addListener(marker, "click", () => {
-//     infowindow.setContent(place.name || "");
-//     infowindow.open(map);
-//   });
-// }
+//     if (!place.geometry || !place.geometry.location) return;
+ 
+//     const marker = new google.maps.Marker({
+//       map,
+//       position: place.geometry.location,
+//     });
+ 
+//     google.maps.event.addListener(marker, "click", () => {
+//       infowindow.setContent(place.name || "");
+//       infowindow.open(map);
+//     });
+//   }
 
 var deleteWarning = function() {
     $('#warning').remove()
 }
-
+ 
 $("#search-form").on("submit", formSubmitHandler);
